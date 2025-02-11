c68201f8577668e108b1f5c2129f7dd2e9249471
Btg1603 <gbuif12@gmail.com>
2025-02-11 21:37:53 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,10 @@
+// khai bao nhhung mau ham 
+
+
+#include "datatype.h"
+ 
+void displayCategoryMenu();
+void showCategories(Category categories[], int count);
+void addCategory(Category categories[], int *count);
+void changeCategory(Category categories[], int count);
+

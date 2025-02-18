b541e52378015703b4e9233ad3010c476ae2b910
Btg1603 <gbuif12@gmail.com>
2025-02-18 08:20:13 +0700

    Delete functionproduct.h

--- functionproduct.h
+++
@@ -1,5 +0,0 @@
-#include "datatype.h"
-
-void displayProductMenu();
-void showProducts(Product products[], int count);
-void addProduct(Product products[], int *count, Category categories[], int categoryCount);
